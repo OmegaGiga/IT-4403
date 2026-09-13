3c01bdc0e345ad625dffd33324c08c8cf1395f4f
Leonardo Mendez-Rivera <89599184+OmegaGiga@users.noreply.github.com>
2026-09-13 14:55:12 -0400

    Created index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>My Website</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple paragraph.</p>
+    </body>
+</html>

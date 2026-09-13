5b13b3d3aff7af706db878802f963e8abde57233
Leonardo Mendez-Rivera <89599184+OmegaGiga@users.noreply.github.com>
2026-09-13 15:18:05 -0400

    Updated index.php
    Added lab 1 required information.

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
     <head>
-        <title>My Website</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Website</title>
     </head>
     <body>
         <h1>Welcome to My Website</h1>
-        <p>This is a simple paragraph.</p>
+        <h3>Leonardo Mendez-Rivera – Kennesaw State University BSIT</h3>
+        <h3>IT 4403 – Advanced Web and Mobile Applications</h3>
+        <p>This site contains assignments and projects for the IT 4403 course, which can be found using the links below.
+            <br>
+            This site will host a web application for demonstrating the concepts learned in the course.</p>
+        <p>Disclaimer: This is a student website created for course projects in IT 4003.</p>
+        <a href="https://campus.kennesaw.edu/colleges-departments/ccse/academics/information-technology/">CCSE Department of IT Website</a>
+        <h3>Links:</h3>
+        <ul>
+            <li><a href="">Exercises</a></li>
+            <li><a href="">Labs</a></li>
+            <li><a href="">Projects</a></li>
+        </ul>
     </body>
 </html>

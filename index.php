4e00171b89cd12e2ca37cec0e9c84277bd88bbe1
Leonardo Mendez-Rivera <89599184+OmegaGiga@users.noreply.github.com>
2026-09-21 15:45:47 -0400

    Fleshed out the wireframe

--- index.php
+++ index.php
@@ -1,24 +1,76 @@
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
+<!DOCTYPE html>
+<html>
+    <head lang="en-us">
         <meta charset="UTF-8">
+        <meta name="author" content="Leonardo Mendez-Rivera">
+        <meta name="description" content="Site for showcasing IT 4403 labs, exercises, and projects.">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Website</title>
+        <title>IT 4403 - Project Showcase</title>
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <h1>Welcome to My Website</h1>
-        <h3>Leonardo Mendez-Rivera – Kennesaw State University BSIT</h3>
-        <h3>IT 4403 – Advanced Web and Mobile Applications</h3>
-        <p>This site contains assignments and projects for the IT 4403 course, which can be found using the links below.
-            <br>
-            This site will host a web application for demonstrating the concepts learned in the course.</p>
-        <p>Disclaimer: This is a student website created for course projects in IT 4003.</p>
-        <a href="https://campus.kennesaw.edu/colleges-departments/ccse/academics/information-technology/">CCSE Department of IT Website</a>
-        <h3>Links:</h3>
-        <ul>
-            <li><a href="">Exercises</a></li>
-            <li><a href="">Labs</a></li>
-            <li><a href="">Projects</a></li>
-        </ul>
+        <div class="window">
+            <header>
+                <div class="title-bar">
+                    <button type="button">x</button>
+                    <button type="button">-</button>
+                    <button type="button">+</button>
+                    <p>Welcome to My Website</p>
+                </div>
+
+                <hr>
+
+                <div class="site-heading">
+                    <h1>Welcome to My Website</h1>
+                    <h2>IT 4403 - Advanced Web and Mobile Applications</h2>
+                    <h2>Leonardo Mendez-Rivera - Kennesaw State University BSIT</h2>
+                </div>
+
+                <nav class="navbar">
+                    <a href="index.html">Home</a>
+                    <a href="#exercises">Exercises</a>
+                    <a href="#labs">Labs</a>
+                    <a href="#projects">Projects</a>
+                </nav>
+            </header>
+            
+            <main class="window-content">
+                <div class="welcome">
+                    <p>
+                        This site contains assignments and projects for the IT 4403 course, which can be found using the links below. 
+                        This site will host a web application for demonstrating the concepts learned in the course.
+                    </p>
+                </div>
+
+                <div class="coursework">
+                    <section id="exercises">
+                        <h3>Exercises</h3>
+                        <ul>
+                            <li>Nothing yet.</li>
+                        </ul>
+                    </section>
+                    <section id="labs">
+                        <h3>Labs</h3>
+                        <ul>
+                            <li>Nothing yet.</li>
+                        </ul>
+                    </section>
+                    <section id="projects">
+                        <h3>Projects</h3>
+                        <ul>
+                            <li>Nothing yet.</li>
+                        </ul>
+                    </section>
+                </div>
+            </main>
+
+            <hr>
+            
+            <footer class="footer">
+                <p><a href="https://campus.kennesaw.edu/colleges-departments/ccse/academics/information-technology/" target="_blank">
+                    CCSE Department of IT Website</a> | 
+                    Disclaimer: This is a student website created for course projects in IT 4403.</p>
+            </footer>
+        </div>
     </body>
 </html>
